f79d374f6dca4258a4645dd201ca667787f9a8f7
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-04-19 17:51:38 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,5 +1,10 @@
 # require 'pry'
+# Here we are setting a variable, collection, equal to an empty array. 
+# Then, inside our while loop, we push the return value of using yield(array[i]) into that collection array.
+#We need to return that new collection at the end of our method
 
+# while loop, yielded each individual member of an array to a block and captured the return values of yielding those items to the block. 
+# This is exactly how the #collect method works, and we've just built it out, all by ourselves.
 def hello(array)
   i = 0
   collection = []
@@ -21,7 +26,12 @@ hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
 # end
 #
 # hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}"
-#
+
+# > Hi, Tim
+# > Hi, Tom
+# > Hi, Jim
+# => nil
+
 # def hello(array)
 #   i = 0
 #   while i < array.length
@@ -32,3 +42,8 @@ hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
 # end
 
 # hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
+
+#Pry console terminal
+#yield(array[i])
+# > "Hi, Tim"
+#yield(some_argument) will give us the return value of the execution of the block with that argument, we can capture those return values.
